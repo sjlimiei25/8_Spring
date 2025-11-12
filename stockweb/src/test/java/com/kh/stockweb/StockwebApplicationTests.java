442a2c2aa1cb874bdb27d9e8e0191c5ef00d4c4d
sjlimiei25 <sjlim.iei.25@gmail.com>
2025-11-12 18:20:58 +0900

    스프링부트 프로젝트 생성

---
+++ stockweb/src/test/java/com/kh/stockweb/StockwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.stockweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
