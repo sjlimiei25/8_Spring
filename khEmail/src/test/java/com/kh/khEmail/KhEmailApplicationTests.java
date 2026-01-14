d41ec72a62588fff5d38d942d98b86137cb116be
sjlimiei25 <sjlim.iei.25@gmail.com>
2026-01-14 16:25:05 +0900

    프로젝트 기본 설정 파일

---
+++ khEmail/src/test/java/com/kh/khEmail/KhEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.khEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
